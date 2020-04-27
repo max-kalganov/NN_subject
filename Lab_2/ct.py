25a22bbe332e402902665b5afef00338dbc68237
max <maksim.kalganov@kreo.net>
2020-04-27 19:07:25 +0300

    finish developping dataset generator

---
+++ Lab_2/ct.py
@@ -0,0 +1,7 @@
+X_COORD = "x_coord"
+Y_COORD = "y_coord"
+CLASSES = "classes"
+
+
+def get_ds_path(name: str) -> str:
+    return f"data/{name}.csv"
--- Lab_2/dataset.py
+++ Lab_2/dataset.py
@@ -1,4 +1,10 @@
+from typing import Tuple
+
 import numpy as np
+from matplotlib import pyplot as plot
+import pandas as pd
+
+from ct import X_COORD, Y_COORD, CLASSES, get_ds_path
 
 
 def create_class(mean: float, st_dev: float, size: int) -> np.array:
@@ -7,9 +13,9 @@ def create_class(mean: float, st_dev: float, size: int) -> np.array:
                             size=(size, 2))
 
 
-def show_classes(ds_cl1: np.array, ds_cl2: np.array):
-    plot.plot(ds_cl1[:, 0], ds_cl1[:, 1], 'bo')
-    plot.plot(ds_cl2[:, 0], ds_cl2[:, 1], 'ro')
+def show_classes(ds: pd.DataFrame):
+    plot.plot(ds[ds[CLASSES] == 0][X_COORD], ds[ds[CLASSES] == 0][Y_COORD], 'bo')
+    plot.plot(ds[ds[CLASSES] == 1][X_COORD], ds[ds[CLASSES] == 1][Y_COORD], 'ro')
     plot.show()
 
 
@@ -17,29 +23,49 @@ def create_class_in_square(min_pos: float, max_pos: float, size: int):
     return np.stack([np.random.random(size)*max_pos + min_pos, np.random.random(size)*max_pos + min_pos], axis=1)
 
 
-if __name__ == '__main__':
-    ds1_cl1 = create_class(12, 2, 100)
-    ds1_cl2 = create_class(0, 2, 100)
+def gen_and_save_datasets():
+    def gen_dataset(args1: Tuple, args2: Tuple) -> Tuple[np.array, np.array]:
+        cl1 = create_class(*args1)
+        cl2 = create_class(*args2)
+        return cl1, cl2
 
-    ds2_cl1 = create_class(12, 2, 1000)
-    ds2_cl2 = create_class(0, 2, 1000)
+    def save_dataset(cl1: np.array, cl2: np.array, size1: int, size2: int, name: str):
+        ds = np.vstack([cl1, cl2])
+        df = pd.DataFrame({X_COORD: ds[:, 0],
+                           Y_COORD: ds[:, 1],
+                           CLASSES: np.concatenate([np.ones(size1), np.zeros(size2)])})
+        df.to_csv(get_ds_path(name))
 
-    ds3_cl1 = create_class(12, 2, 300)
-    ds3_cl2 = create_class(0, 2, 50)
+    def gen_and_save_dataset(args1: Tuple, args2: Tuple, name: str):
+        cl1, cl2 = gen_dataset(args1, args2)
+        save_dataset(cl1, cl2, args1[-1], args2[-1], name)
 
-    ds4_cl1 = create_class(6, 2, 300)
-    ds4_cl2 = create_class(0, 2, 50)
-
-    ds5_cl1 = create_class(3.5, 4, 300)
-    ds5_cl2 = create_class(0, 2, 50)
+    gen_and_save_dataset((12, 2, 100), (0, 2, 100), "ds1")
+    gen_and_save_dataset((12, 2, 1000), (0, 2, 1000), "ds2")
+    gen_and_save_dataset((12, 2, 300), (0, 2, 50), "ds3")
+    gen_and_save_dataset((6, 2, 300), (0, 2, 50), "ds4")
+    gen_and_save_dataset((3.5, 4, 300), (0, 2, 50), "ds5")
+    gen_and_save_dataset((12, 2, 100), (0, 2, 100), "ds1")
 
     ds6_cl1 = create_class_in_square(1, 2, 100)
     ds6_cl2 = create_class(0, 2, 100)
+    save_dataset(ds6_cl1, ds6_cl2, 100, 100, "ds6")
 
-    from matplotlib import pyplot as plot
-    show_classes(ds1_cl1, ds1_cl2)
-    show_classes(ds2_cl1, ds2_cl2)
-    show_classes(ds3_cl1, ds3_cl2)
-    show_classes(ds4_cl1, ds4_cl2)
-    show_classes(ds5_cl1, ds5_cl2)
-    show_classes(ds6_cl1, ds6_cl2)
+
+def show_ds(name):
+    df = pd.read_csv(get_ds_path(name))
+    show_classes(df)
+
+
+def show_datasets():
+    show_ds("ds1")
+    show_ds("ds2")
+    show_ds("ds3")
+    show_ds("ds4")
+    show_ds("ds5")
+    show_ds("ds6")
+
+
+if __name__ == '__main__':
+    gen_and_save_datasets()
+    show_datasets()
